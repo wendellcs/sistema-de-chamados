8fad2641dd26419de293f982d90f9e6ed08880f5
Wendell <wendellcs131@gmail.com>
2024-02-05 23:14:22 -0300

    Add dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,11 @@
 import { AuthContext } from "../../contexts/auth"
 import { useContext } from "react"
 import Header from "../../components/Header"
+import Title from "../../components/Title"
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi"
+import { Link } from "react-router-dom"
+
+import './dashboard.css'
 
 export default function Dashboard() {
     const { logOut } = useContext(AuthContext)
@@ -12,7 +17,42 @@ export default function Dashboard() {
     return (
         <div>
             <Header />
-            <h1>Dashboard</h1>
+            <div className="content">
+                <Title name='tickets'>
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to={'/new'} className="new">
+                        <FiPlus color="#fff" size={25} />
+                        Novo chamado
+                    </Link>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label='cliente'>Mercado Esquina</td>
+                                <td data-label='Assunto'>Suporte</td>
+                                <td data-label='Status'>Em aberto</td>
+                                <td data-label='Cadastrado'>12/05/2022</td>
+                                <td data-label='#'>
+                                    <button className="action" style={{ backgroundColor: '#3583f6' }}><FiSearch color="#fff" size={17} /></button>
+                                    <button className="action" style={{ backgroundColor: '#f6a935' }}><FiEdit2 color="#fff" size={17} /></button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+
+            </div>
             <button onClick={handleLogOut}>Sair</button>
         </div>
     )
