8fe7ad8e18c8de2482f15a7d67d6f12b75067be3
Wendell <wendellcs131@gmail.com>
2024-02-08 23:07:42 -0300

    Add Page 'Novo Chamado'

---
+++ src/pages/New/index.js
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import Header from '../../components/Header'
+import Title from '../../components/Title'
+import { FiPlusCircle } from 'react-icons/fi'
+import './new.css'
+
+export default function New() {
+
+    const [costumers, setCostumers] = useState([])
+
+    const [complemento, setComplemento] = useState('')
+    const [assunto, setAssunto] = useState('suporte')
+    const [status, setStatus] = useState('aberto')
+
+    function handleOptionChange(e) {
+        setStatus(e.target.value)
+
+
+    }
+
+    return (
+        <div>
+            <Header />
+
+            <div className='content'>
+                <Title text='Novo chamado'>
+                    <FiPlusCircle size={25} />
+                </Title>
+
+                <div className='container'>
+                    <form className='form-profile'>
+                        <label>Clientes</label>
+                        <select>
+                            <option key={1} value={1}>Mercado Teste</option>
+                            <option key={2} value={2}>Loja Teste</option>
+                            <option key={3} value={3}>Shopping Teste</option>
+                            <option key={4} value={4}>Igreja Teste</option>
+                        </select>
+
+                        <label>Assunto</label>
+                        <select>
+                            <option value='suporte'>Suporte</option>
+                            <option value='visita_tecnica'>Visita Tecnica</option>
+                            <option value='financeiro'>Financeiro</option>
+                        </select>
+
+                        <label>Status</label>
+                        <div className='status'>
+                            <input type='radio' name='radio' value='aberto' onChange={handleOptionChange} checked={status === 'aberto'} />
+                            <span>Em aberto</span>
+
+                            <input type='radio' name='radio' value='progresso' onChange={handleOptionChange} checked={status === 'progresso'} />
+                            <span>Em progresso</span>
+
+                            <input type='radio' name='radio' value='atendido' onChange={handleOptionChange} checked={status === 'atendido'} />
+                            <span>Atendido</span>
+                        </div>
+
+                        <label>Complemento</label>
+                        <textarea value={complemento} onChange={(e) => { setComplemento(e.target.value) }} typeof='text' placeholder='Descreva seu problema ( Opcional )' />
+                        <button type='submit'>Registrar</button>
+                    </form>
+
+                </div>
+            </div>
+        </div >
+    )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 import Private from './Private';
 import Costumers from '../pages/Costumers';
+import New from '../pages/New';
 
 function RoutesApp() {
     return (
@@ -13,9 +14,11 @@ function RoutesApp() {
             <Route path="/" element={<SignIn />} />
             <Route path="/register" element={<SignUp />} />
 
+            {/* Private */}
             <Route path="/dashboard" element={<Private> <Dashboard /> </Private>} />
             <Route path="/profile" element={<Private> <Profile /> </Private>} />
             <Route path="/costumers" element={<Private> <Costumers /> </Private>} />
+            <Route path="/new" element={<Private> <New /> </Private>} />
         </Routes>
 
     );
