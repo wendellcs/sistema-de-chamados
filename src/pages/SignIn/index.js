ff38f516dd182596fe577fa5fe62650ccaafb223
Wendell <wendellcs131@gmail.com>
2024-03-13 22:26:38 -0300

    Change file name

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,28 +1,28 @@
 import { useState, useContext } from 'react'
-import './signin.css'
+import './signIn.css'
 
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../contexts/auth'
 
 
-export default function SignIn(){
+export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const { signIn, loadingAuth } = useContext(AuthContext)
 
-  async function handleSignIn(e){
+  async function handleSignIn(e) {
     e.preventDefault();
 
-    if(email !== '' && password !== ''){
+    if (email !== '' && password !== '') {
       await signIn(email, password);
     }
 
   }
 
 
-  return(
+  return (
     <div className="container-center">
       <div className="login">
         <div className="login-area">
@@ -31,18 +31,18 @@ export default function SignIn(){
 
         <form onSubmit={handleSignIn}>
           <h1>Entrar</h1>
-          <input 
-            type="text" 
+          <input
+            type="text"
             placeholder="email@email.com"
             value={email}
-            onChange={ (e) => setEmail(e.target.value) }
+            onChange={(e) => setEmail(e.target.value)}
           />
 
-          <input 
-            type="password" 
+          <input
+            type="password"
             placeholder="********"
             value={password}
-            onChange={ (e) => setPassword(e.target.value) }
+            onChange={(e) => setPassword(e.target.value)}
           />
 
           <button type="submit">
